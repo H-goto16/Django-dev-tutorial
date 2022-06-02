07e1881dcb3b13f2cfc7749763f3f0626fa86b7d
Haruki Goto <97876815+H-goto16@users.noreply.github.com>
2022-06-02 20:15:42 +0900

    Delete Django/mysite directory

--- Django/mysite/website/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-
-# Create your views here.
-class IndexView(TemplateView):
-    template_name = "index.html"
-    
-class AboutView(TemplateView):
-    template_name = "about.html"
